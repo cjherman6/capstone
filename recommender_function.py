20d43ef7a3e09859910c388d8401b77cd877f3dd
Christopher Herman <Chris@Christophers-MacBook-Air.local>
2018-12-21 14:29:53 -0700

    working on survey questions

--- recommender_function.py
+++ recommender_function.py
@@ -4,7 +4,7 @@ from sklearn.metrics.pairwise import euclidean_distances,cosine_similarity
 
 df = pd.read_csv('app_data/breed_traits.csv',index_col='Unnamed: 0')
 
-def overall_recommender(breed,dist='euclidean'):
+def overall_recommender(breed,dist='cosine'):
     '''
     Input: Name of breed (string)
     Output: 5 Breeds with most similar temperaments according to dogtime.com ratings
@@ -17,9 +17,9 @@ def overall_recommender(breed,dist='euclidean'):
     if dist == 'euclidean':
         return [df.iloc[ind,:].name for ind in euc_ind][1:6]
     elif dist == 'cosine':
-        return [df.iloc[ind,:].name for ind in cos_ind][1:6]
+        return [df.iloc[ind,:].name for ind in cos_ind][-1:-6:-1]
 
-def predictions_recommender(breed,photo_list,dist='euclidean'):
+def predictions_recommender(breed,photo_list,dist='cosine'):
     '''
     Input: Name of breed (string), List of dogs you're considering (list)
     Output: Ordered list starting from most similar to least
@@ -33,4 +33,19 @@ def predictions_recommender(breed,photo_list,dist='euclidean'):
     if dist == 'euclidean':
         return [X.iloc[ind,:].name for ind in euc_ind]
     elif dist == 'cosine':
-        return [X.iloc[ind,:].name for ind in cos_ind]
+        return [X.iloc[ind,:].name for ind in cos_ind][::-1]
+
+def profile_recommender(profile,dist='cosine'):
+    '''
+    Input: Profile created from radio inputs (np array)
+    Output: 5 Breeds with most similar temperaments according to dogtime.com ratings
+    '''
+    y = profile
+    euc_dists = euclidean_distances(df.values,y)
+    euc_ind = np.argsort(euc_dists.flatten())
+    cos_dists = cosine_similarity(df.values,y)
+    cos_ind = np.argsort(cos_dists.flatten())
+    if dist == 'euclidean':
+        return [df.iloc[ind,:].name for ind in euc_ind][1:6]
+    elif dist == 'cosine':
+        return [df.iloc[ind,:].name for ind in cos_ind][-1:-6:-1]
