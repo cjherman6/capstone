9e8a135496b925698e7234572ff85eea6a9e9663
Christopher Herman <Chris@Christophers-MacBook-Air.local>
2019-01-02 13:41:38 -0700

    m

--- radio_buttons/recommender_function.py
+++ radio_buttons/recommender_function.py
@@ -41,9 +41,10 @@ def profile_recommender(profile,photo_list,dist='cosine'):
     Output: 5 Breeds with most similar temperaments according to dogtime.com ratings
     '''
     y = profile
-    euc_dists = euclidean_distances(df.values,y)
+    X = df.loc[photo_list,:]
+    euc_dists = euclidean_distances(X.values,y)
     euc_ind = np.argsort(euc_dists.flatten())
-    cos_dists = cosine_similarity(df.values,y)
+    cos_dists = cosine_similarity(X.values,y)
     cos_ind = np.argsort(cos_dists.flatten())
     if dist == 'euclidean':
         return [df.iloc[ind,:].name for ind in euc_ind][1:6]
